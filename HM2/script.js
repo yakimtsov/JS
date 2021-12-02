2ed13055b758c760eb32535389c588965e95f31d
Yakimtsov2 <yakimtsov2@gmail.com>
2021-12-02 14:58:48 +0200

    finish hm2

---
+++ HM2/script.js
@@ -0,0 +1,27 @@
+let UserAge = 17;
+console.log(UserAge);
+
+let UserName = "Ross";
+console.log(UserName);
+
+let UserSurname = "Klinton";
+console.log(UserSurname);
+
+let $newUser = "nickname";
+
+console.log($newUser);
+
+let $ShoppingList = ["icecream", 12];
+console.log($ShoppingList);
+
+let $UserStatus = true;
+console.log($UserStatus);
+
+let WorkerSalary = 2000;
+console.log(WorkerSalary);
+
+let MouseEvent = 120;
+console.log(MouseEvent);
+
+let UserLocation = "Hillton";
+console.log("element UserLocation", UserLocation)
