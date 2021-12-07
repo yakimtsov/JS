91492bb9bc2a12861c62ccdf490cdf27094110b2
Yakimtsov2 <yakimtsov2@gmail.com>
2021-12-07 21:39:00 +0200

    finish the first task

---
+++ hm4/script.js
@@ -0,0 +1,22 @@
+// First task
+const a = 34;
+const b = 23;
+
+function sumNumbers(a , b){
+    const sum = a + b;
+    console.log('sum: ', sum );
+}
+
+sumNumbers(a, b);
+sumNumbers(2, 1);
+sumNumbers(70, 56);
+
+const summary = (a, b) => a + b;
+    const firstResult = summary(23, 67);
+    const secondResult = summary(78, 890);
+    const thirdResult = summary(567, 67);
+
+
+console.log(firstResult);
+console.log(secondResult);
+console.log(thirdResult);
