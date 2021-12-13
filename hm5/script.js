106c7f94a5f95b0a570641b3d96364c08cdcf696
Yakimtsov2 <yakimtsov2@gmail.com>
2021-12-13 14:05:00 +0200

    Task 2

--- hm5/script.js
+++ hm5/script.js
@@ -36,3 +36,26 @@ const authorize = () => {
     } while (!isAuthSuccess && i == 0)
 }
 authorize();
+
+
+
+// Task n2
+
+const lowerNumber = Number(prompt('Enter lower number: '));
+const higherNumber = Number(prompt('Enter higher number: '));
+
+console.log(`The prime numbers between ${lowerNumber} and ${higherNumber} are:`);
+
+for (let i = lowerNumber; i <= higherNumber; i++) {
+    let flag = 0;
+
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+    }
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
+}
